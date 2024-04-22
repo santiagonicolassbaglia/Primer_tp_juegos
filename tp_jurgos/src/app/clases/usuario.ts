edbf6115d5ae067fe6d463fe680fa749c8edacdc
santiagonicolassbaglia <santiagosbaglia@hotmail.com>
2024-04-21 21:18:51 -0300

    tp juegos

---
+++ tp_jurgos/src/app/clases/usuario.ts
@@ -0,0 +1,12 @@
+export class Usuario {
+
+    public nombre: string;
+    public clave: string;
+
+    constructor(nombre: string, clave: string) {
+        this.nombre = nombre;
+        this.clave = clave;
+    }
+
+    
+}
---
+++ tp_jurgos/src/app/componentes/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Usuario } from '../../clases/usuario';
+import { FormsModule } from '@angular/forms';
+import { PaginaErrorComponent } from '../pagina-error/pagina-error.component';
+import { NgIf } from '@angular/common';
+import { HomeComponent } from '../home/home.component';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, HomeComponent, PaginaErrorComponent, NgIf],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  nombre: string = '';
+  clave: string = '';
+resultado: boolean = false;
+  submitForm() {
+ 
+    const usuario = new Usuario(this.nombre, this.clave);
+  }
+
+  login() {
+    if (this.nombre === 'santi' && this.clave === '123') {
+      this.resultado = true;
+    } else {
+      this.resultado = false;
+    }
+  }
+}
