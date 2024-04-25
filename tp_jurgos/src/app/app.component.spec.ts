58bf51f98ca2b07ef924a1ad7a6446897f5bde24
santiagonicolassbaglia <santiagosbaglia@hotmail.com>
2024-04-25 19:27:28 -0300

    .

--- tp_jurgos/src/app/app.component.spec.ts
+++ tp_jurgos/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'tp_jurgos' title`, () => {
+  it(`should have the 'tp-juegos' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tp_jurgos');
+    expect(app.title).toEqual('tp-juegos');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tp_jurgos');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tp-juegos');
   });
 });
--- tp_jurgos/src/app/app.component.ts
+++ tp_jurgos/src/app/app.component.ts
@@ -13,5 +13,5 @@ import { PresentacionComponent } from './presentacion/presentacion.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tp_jurgos';
+  title = 'tp-juegos';
 }
