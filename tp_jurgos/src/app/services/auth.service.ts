89a32fc4248135d0b286fbb899145263593a733e
santiagonicolassbaglia <santiagosbaglia@hotmail.com>
2024-05-08 01:57:27 -0300

    auth serve.. funcionando

--- tp_jurgos/src/app/services/auth.service.ts
+++ tp_jurgos/src/app/services/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable, inject } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, UserCredential } from 'firebase/auth'; // Modificado
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,UserCredential} from 'firebase/auth'; // Modificado
 import { Usuario } from '../clases/usuario';
 import { Observable } from 'rxjs';
 import {AngularFireDatabase} from '@angular/fire/compat/database';
-
+ 
+ 
 @Injectable({
   providedIn: 'root'
 })
@@ -27,39 +28,70 @@ export class AuthService {
     return this.items$;
   }
 
-
  
 
+//   async registrar(objeto: Usuario) {
+//     try {console.log('entroooo');
+//       objeto.code = this.db.createPushId().substring(0, 10);
+//       console.log('entroooo2');
+//       const path = `${this.PATH}/${objeto.code}`;
+//       console.log('Registrando usuario en Firebase...');
+// await this.db.object(path).set(objeto);
+//       console.log('entroooo3');
+//       console.log('Usuario registrado correctamente');
+//     } catch (error) {
+//       console.error('Error al registrar usuario en Firebase:', error);
+//       throw error; // Lanzar el error para manejarlo en el componente
+//     }
+//   }
 
-  async registrar(objeto: Usuario) {
-    try {console.log('entroooo');
-      objeto.code = this.db.createPushId().substring(0, 10);
-      console.log('entroooo2');
-      const path = `${this.PATH}/${objeto.code}`;
-      console.log('Registrando usuario en Firebase...');
-await this.db.object(path).set(objeto);
-      console.log('entroooo3');
-      console.log('Usuario registrado correctamente');
-    } catch (error) {
-      console.error('Error al registrar usuario en Firebase:', error);
-      throw error; // Lanzar el error para manejarlo en el componente
-    }
+// async registrar(objeto: Usuario) {
+//   try {
+ 
+//     const userCredential: UserCredential = await this.auth.createUserWithEmailAndPassword(objeto.mail, objeto.contraseña);
+//     const uid = userCredential.user.uid;
+ 
+//     await this.guardarUsuarioFirestore(uid, objeto.nombre, objeto.mail);
+//     console.log('Usuario registrado correctamente');
+//   } catch (error) {
+//     console.error('Error al registrar usuario en Firebase:', error);
+//     throw error; // Lanzar el error para manejarlo en el componente
+//   }
+// }
+
+async registrar(objeto: Usuario) {
+  try {
+    // @ts-ignore
+    const userCredential: UserCredential = await this.auth.createUserWithEmailAndPassword(objeto.mail, objeto.contraseña);
+    const uid = userCredential.user.uid;
+    // Guardar datos adicionales del usuario en Firestore
+    await this.guardarUsuarioFirestore(uid, objeto.nombre, objeto.mail);
+    console.log('Usuario registrado correctamente');
+  } catch (error) {
+    console.error('Error al registrar usuario en Firebase:', error);
+    throw error; // Lanzar el error para manejarlo en el componente
   }
+}
 
   async login(mail: string, pass: string) {
     try {
       await this.auth.signInWithEmailAndPassword(mail, pass);
     } catch (error) {
+      console.error('Error de inicio de sesión:', error);
       throw error;
     }
   }
 
-  private async guardarUsuarioFirestore(uid: string, nombre: string, email: string) {
-    await this.firestore.collection('usuarios').doc(uid).set({
+  public async guardarUsuarioFirestore(uid: string, nombre: string, email: string) {
+    await this.firestore.collection('Usuarios').doc(uid).set({
       nombre,
       email
     });
   }
+
+
+
+ 
 }
 
 //   constructor(private authF: AngularFireAuth) { }
